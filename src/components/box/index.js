c0904d20e6809439506b8de40c4b1811310ac774
Radek Stangel <git@radekstangel.com>
2018-01-17 10:32:30 +0100

    Add styled div as a page container

---
+++ src/components/box/index.js
@@ -0,0 +1,16 @@
+// dependencies
+import styled from 'styled-components';
+
+const Box = styled.div`
+  padding: 0 1em;
+  margin: 0 auto;
+  max-width: 968px;
+  min-width: 320px;
+  width: 100%;
+
+  @media (min-width: 680px) {
+    padding: 0 2em;
+  }
+`;
+
+export default Box;
