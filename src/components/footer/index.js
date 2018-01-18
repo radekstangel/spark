aed9bd137172add43938513ddc74a266403504ad
Radek Stangel <git@radekstangel.com>
2018-01-18 10:33:28 +0100

    Create footer DOM

---
+++ src/components/footer/index.js
@@ -0,0 +1,16 @@
+// dependencies
+import React from 'react';
+
+// components
+import Box from '../box';
+import Wrapper from './wrapper';
+
+const Footer = () => (
+  <Wrapper>
+    <Box>
+      Copyright &copy; 2016 Habitzilla. All rights reserved.
+    </Box>
+  </Wrapper>
+);
+
+export default Footer;
