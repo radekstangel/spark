643f834007ee6341a474584afcb077ed8db08821
Radek Stangel <git@radekstangel.com>
2018-01-18 10:33:48 +0100

    Style the footer

---
+++ src/components/footer/wrapper.js
@@ -0,0 +1,21 @@
+// dependencies
+import styled from 'styled-components';
+
+// styles
+import { color, media } from '../../styles/theme';
+
+const Footer = styled.footer`
+  background: ${color.primaryDark};
+  color: ${color.white};
+  font-size: 0.875rem;
+  line-height: 1.4;
+  position: relative; /* to make z-index work */
+  z-index: 1; /* above off-canvas */
+  padding: 0.75em 0;
+
+  ${media.medium} {
+    font-size: 0.9375rem;
+  }
+`;
+
+export default Footer;
