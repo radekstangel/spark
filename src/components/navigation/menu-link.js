da59bc4848f41a7623c9bb7164e92051dce7c537
Radek Stangel <git@radekstangel.com>
2018-01-31 10:51:07 +0100

    Style menu link

---
+++ src/components/navigation/menu-link.js
@@ -0,0 +1,25 @@
+// dependencies
+import styled from 'styled-components';
+
+// components
+import NavLink from './nav-link';
+
+// styles
+import { color, media, nav } from '../../styles/theme';
+
+const MenuLink = styled(NavLink)`
+  color: ${color.white};
+  padding: 0 1rem;
+  text-align: center;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.15);
+    color: #fff;
+  }
+
+  ${media.large} {
+    padding: 0 1.75rem;
+  }
+`;
+
+export default MenuLink;
